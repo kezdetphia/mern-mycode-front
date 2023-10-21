3e34cea9a91e451bdae7f76f71e1ecd5d42f5980
kezdetphia <fehermark88@gmail.com>
2023-10-20 20:03:36 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#41346F",
+        darkblue: "#002A52",
+        textcolor: "#525252",
+        darkgray: "#002A52",
+        lightgray: "#474747",
+      },
+    },
+  },
+  plugins: [],
+};
