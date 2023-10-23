92a7c31c2ebd7de131343a050a5c6bea8dc592df
kezdetphia <fehermark88@gmail.com>
2023-10-22 23:05:48 -0700

    error handling for unfilled fields in workoutform, higlight red the unfilled fields, red border should disappear when start typing

--- src/Components/WorkoutForm.jsx
+++ src/Components/WorkoutForm.jsx
@@ -1,48 +1,62 @@
 import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
-
 const WorkoutForm = () => {
-  const {state, dispatch} = useWorkoutsContext()
-  
-  const initialForm = {
-    title: '',
-    reps: '',
-    load: ''
-  }
-  
-  const [workout, setWorkout] = useState(initialForm)
+  const { state, dispatch } = useWorkoutsContext();
 
-  const handleSubmit = async(e) =>{
-    try{
-      e.preventDefault()
+  const initialForm = {
+    title: "",
+    reps: "",
+    load: "",
+  };
+
+  const classStyle = (name) => {
+    return `mb-7 pl-1  ${
+      (emptyFields.includes(name)) && " border shadow-sm shadow-red-500 border-red-500"}`;
+  };
+
+  const [workout, setWorkout] = useState(initialForm);
+  const [error, setError] = useState("");
+  const [emptyFields, setEmptyFields] = useState([]);
+  const [focusedField, setFocusedField] = useState("");
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
       const res = await fetch("/api/workouts", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(workout),
       });
-      
+
       const resData = await res.json();
-      
-      if (res.ok){
+
+      if (!res.ok) {
+        setError(resData.error);
+        setEmptyFields(resData.emptyFields);
+      }
+
+      if (res.ok) {
         setWorkout(initialForm);
-        console.log('new workout added', resData)
+        console.log("new workout added", resData);
+        setEmptyFields([]);
+        setError(null);
         dispatch({ type: "CREATE_WORKOUT", payload: resData });
       } else {
-        console.error('Request failed with status: ', res.status)
+        console.error("Request failed with status: ", res.status);
       }
-    }catch(err){
-      console.log("Error:", err)
-    }   
-  }
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  };
 
- const handleChange = (e)=>{
-  const {name,value} = e.target
-  setWorkout({...workout, [name]:value})
- }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setWorkout({ ...workout, [name]: value });
+  };
 
   return (
-    <div className="flex flex-col h-[400px] mr-10   shadow-lg shadow-darkgray  ">
+    <div className="flex flex-col mr-10   shadow-lg shadow-darkgray  ">
       <p className="mx-7 my-4 text-xl font-bold">Add a new workout</p>
       <form
         className="flex flex-col mx-7 my-4   "
@@ -55,7 +69,12 @@ const WorkoutForm = () => {
           value={workout.title}
           name="title"
           onChange={handleChange}
-          className="mb-7 pl-1"
+          className={`mb-7 pl-1 ${
+            emptyFields.includes("title") && focusedField !== "title"
+              ? "border shadow-sm shadow-red-500 border-red-500"
+              : "border-transparent"
+          }`}
+          onFocus={() => setFocusedField("title")}
         />
         <label className="text-lightgray pb-2">Load: (in kg)</label>
         <input
@@ -63,7 +82,12 @@ const WorkoutForm = () => {
           value={workout.load}
           name="load"
           onChange={handleChange}
-          className="mb-7 pl-1"
+          className={`mb-7 pl-1 ${
+            emptyFields.includes("load") && focusedField !== "load"
+              ? "border shadow-sm shadow-red-500 border-red-500"
+              : "border-black"
+          }`}
+          onFocus={() => setFocusedField("load")}
         />
         <label className="text-lightgray pb-2">Reps: </label>
         <input
@@ -71,7 +95,12 @@ const WorkoutForm = () => {
           value={workout.reps}
           name="reps"
           onChange={handleChange}
-          className="mb-7 pl-1"
+          className={`mb-7 pl-1 ${
+            emptyFields.includes("reps") && focusedField !== "reps"
+              ? "border shadow-sm shadow-red-500 border-red-500"
+              : "border-black"
+          }`}
+          onFocus={() => setFocusedField("reps")}
         />
 
         <div className="flex justify-start items-center ">
@@ -82,6 +111,7 @@ const WorkoutForm = () => {
             Add Workout
           </button>
         </div>
+        <div className="pt-4 text-red-500">{error}</div>
       </form>
     </div>
     // </div>
