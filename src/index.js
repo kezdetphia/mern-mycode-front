9481e1ff2105d3a044aeef42cb22baf136fd213f
kezdetphia <fehermark88@gmail.com>
2023-10-30 19:31:52 -0700

    wrap index.js with  codesContextProvider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutsContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { WorkoutsContextProvider } from "./context/WorkoutsContext";
 import { AuthContextProvider } from "./context/AuthContext";
+import { CodesContextProvider } from "./context/CodesContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <WorkoutsContextProvider>
-        <App />
-      </WorkoutsContextProvider>
+      <CodesContextProvider>
+        <WorkoutsContextProvider>
+          <App />
+        </WorkoutsContextProvider>
+      </CodesContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
